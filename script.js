6404e2c8d33f3c2383f5196cd199170de44c585f
Julius <julius@ieeegec.org>
2019-04-29 10:13:24 +0530

    Added Scrolling animation to "To Top button"
    https://github.com/ieee-gec-sb/Project-Oslo/pull/13#discussion_r279201648

--- script.js
+++ script.js
@@ -11,8 +11,25 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    scrollTo(document.body, document.body.offsetTop, 500);
+    scrollTo(document.documentElement, document.documentElement.offsetTop, 30);
 }
 
+/**
+ * Adapted from https://stackoverflow.com/questions/41571310/scroll-to-top-link-for-website-using-pure-javascript-withoutjquery
+ * @param {document.element} element Element to scrool to
+ * @param {Number} pos Relative offset to element
+ * @param {Number} duration total time of animation
+ */
+function scrollTo( element, pos, duration) {
+    if (duration < 0) return
+    
+    let distance = pos - element.scrollTop;
+    let perTick  = distance / duration * 2;
+
+    setTimeout( () => {
+        element.scrollTop = element.scrollTop + perTick;
+        scrollTo(element, pos , duration - 2);
+    }, 10);
+}
 
